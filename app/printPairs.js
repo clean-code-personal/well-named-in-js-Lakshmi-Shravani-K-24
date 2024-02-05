aa1bd05ab4947ecb6c5ee8621783d4c609e522e0
Lakshmi Shravani K <132111824+Lakshmi-Shravani-K-24@users.noreply.github.com>
2024-02-05 21:18:19 +0530

    Update printPairs.js

--- app/printPairs.js
+++ app/printPairs.js
@@ -1,19 +1,15 @@
 const { MajorColorNames, MinorColorNames } = require('./colorCoder');
-
 function printColorPairs() {
   console.log("Color Code and Pairs Manual");
   let colorCoding = '';
-
   for (let i = 0; i < MajorColorNames.length; i++) {
     for (let j = 0; j < MinorColorNames.length; j++) {
       let pairNumber = i * MinorColorNames.length + (j + 1);
       colorCoding += `Pair Number: ${pairNumber}, MajorColor: ${MajorColorNames[i]}, MinorColor: ${MinorColorNames[j]}\n`;
     }
   }
-
   return colorCoding;
 }
-
 module.exports = {
   printColorPairs,
 };
