713e4d3d98cfc1a38a59eef3c88581eac98d8892
Lakshmi Shravani K <132111824+Lakshmi-Shravani-K-24@users.noreply.github.com>
2024-02-06 20:25:17 +0530

    Update main.js

--- app/main.js
+++ app/main.js
@@ -5,11 +5,13 @@ const {
 const { printColorPairs, ColorPair } = require("./printPairs");
 function test() {
   pairNumber = 4;
+  console.assert(pairNumber != 0);
   let testPair1 = getColorFromPairNumber(pairNumber);
   console.log(`[In]Pair Number: ${pairNumber},[Out] Colors:${testPair1}`);
   console.assert(testPair1.majorColor == "WHITE");
   console.assert(testPair1.minorColor == "BROWN");
   pairNumber = 5;
+  console.assert(pairNumber != 0);
   testPair1 = getColorFromPairNumber(pairNumber);
   console.log(`[In]Pair Number: ${pairNumber},[Out] Colors:${testPair1}`);
   console.assert(testPair1.majorColor == "WHITE");
@@ -31,6 +33,14 @@ function test() {
   pairNumber = getPairNumberFromColor(testPair2);
   console.log(`[In]Colors: ${testPair2}, [Out] PairNumber: ${pairNumber}`);
   console.assert(pairNumber == 6);
+  console.assert(() => {
+    try {
+      getColorFromPairNumber(20);
+      return false;
+    } catch (error) {
+      return true;
+    }
+  }, "Test case 1 failed: Expected no error for valid pair number.");
 }
 test();
 console.log(printColorPairs());
